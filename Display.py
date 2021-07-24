39969fadef3b16a25f319645085b542d9a2f6fcf
Jespris <87135834+Jespris@users.noreply.github.com>
2021-07-24 21:18:08 +0300

    Add files via upload

---
+++ Display.py
@@ -0,0 +1,10 @@
+"""
+Main displaying file, responsible for:
+- Displaying the board
+- Displaying the pieces
+- Displaying the movelog
+- Displaying time
+- Displaying eval bar
+- Animating moves
+- Displaying legal squares
+"""
